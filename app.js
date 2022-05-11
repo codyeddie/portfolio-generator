a10673efd84e8dee96f3b8457332140f5e861024
Cody Eddie <buffbuck0427@gmail.com>
2022-05-10 21:03:05 -0600

    add question object to app.js

--- app.js
+++ app.js
@@ -1,25 +1,22 @@
-const fs = require('fs');
+const inquirer = require('inquirer');
 
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name'
+        }
+    ])
+    .then(answers => console.log(answers));
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-// const printProfileData = profileDataArr => {
-//     // for (let i = 0; i < profileDataArr.length; i++) {
-//     //     console.log(profileDataArr[i]);
-//     // }
-//     // the forEach statement operates the sane as the for loop that has been commented out above
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
-// printProfileData(profileDataArgs);
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//     if (err) throw err;
 
-// console.log(name, github)
-// console.log(generatePage(name, github));
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-    if (err) throw err;
-
-    console.log('Portfolio complete! Checkout index.html to see the output!');
-});
+//     console.log('Portfolio complete! Checkout index.html to see the output!');
+// });
