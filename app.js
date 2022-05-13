2be5625fed82d94f7a855d18c069429cbd943338
Cody Eddie <buffbuck0427@gmail.com>
2022-05-12 23:59:02 -0600

    refactor copyfile and writefile methods into new dist folder

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const { type } = require('os');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
 const promptUser = () => {
@@ -137,22 +137,20 @@ Add a New Project
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        //changing portfolio data to mockdata temp
-        const pageHTML = generatePage(portfolioData);
-
-        fs.writeFile('./dist/index.html', generatePage(portfolioData), err => {
-            if (err) throw err;
-
-            console.log('Portfolio complete! Checkout index.html to see the output!');
-
-            fs.copyFile('./src/style.css', './dist/style.css', err => {
-                if (err) {
-                    console.log(err);
-                    return;
-                }
-                console.log('Style sheet copied successfully!');
-            })
-        });
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
     });
 
 
